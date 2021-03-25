f3100a8631ef6e0dd06c61c79979a79217870eec
Oscar Corea <rafaelrojas2011@hotmail.com>
2021-03-24 23:04:44 -0600

    first commit

---
+++ src/components/ShowZip.js
@@ -0,0 +1,36 @@
+import './ShowZip.css';
+import React from 'react'
+
+export default function ShowZip({ data, zip, coincidencias }) {
+
+    if (zip.length < 5 && zip.length > 0 && coincidencias.length == 0) {
+        return (
+            <div className='container'>
+                <p>No se encontraron coincidencias, prueba con otro código zip!</p>
+            </div>
+        )
+    }
+
+    if (data === undefined || zip.length < 5) {
+        return null
+    }
+
+    if (data === null) {
+        return (
+            <div className='container'>
+                <p>No se encontraron coincidencias, prueba con otro código zip!</p>
+            </div>
+        )
+    }
+
+    return (
+        <div className='container'>
+            <p><span>Postal Code: </span>{ data.PostalCode }</p>
+            <p><span>Department: </span>{ data.Department }</p>
+            <p><span>Municipality: </span>{ data.Municipality }</p>
+            <p><span>Neighbourhood: </span>{ data.Neighbourhood }</p>
+        </div>
+    )
+}
+
+
